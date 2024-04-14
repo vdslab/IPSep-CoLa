7ef6bcb62b744df92708d915efeac05e158b6b7a
itochs <halglobe0108@gmail.com>
2024-04-15 06:21:51 +0900

    violationで無限ループ

--- src/ipsep_cola/main.py
+++ src/ipsep_cola/main.py
@@ -5,6 +5,8 @@ import os
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
+from block import Block, NodeBlocks
+from constraint import Constraints
 from majorization.main import (
     stress,
     weight_laplacian,
@@ -14,14 +16,12 @@ from majorization.main import (
 from networkx import floyd_warshall_numpy
 from scipy.sparse.linalg import cg
 
-from .block import Block, NodeBlocks
-from .constraint import Constraints
-
 lm = dict()
 blocks = [i for i in range(1)]
 offset = [0 for _ in range(1)]
 x = [[0] for _ in range(1)]
 B = [Block(i, x[i][0]) for i in range(1)]
+G = nx.Graph()
 
 
 def left(ci):
@@ -44,8 +44,10 @@ def posn(vi):
     return B[blocks[vi]].posn + offset[vi]
 
 
-def violation(ci):
-    return posn(left(ci)) + gap(ci) - posn(right(ci))
+def violation(ci, constraints: Constraints, node_blocks: NodeBlocks):
+    ci_left = constraints.left(ci)
+    ci_right = constraints.right(ci)
+    return node_blocks.posn(ci_left) + constraints.gap(ci) - node_blocks.posn(ci_right)
 
 
 def solve_QPSC(A, b, constraints: Constraints, node_blocks: NodeBlocks):
@@ -62,7 +64,8 @@ def solve_QPSC(A, b, constraints: Constraints, node_blocks: NodeBlocks):
         no_split = split_blocks(constraints, node_blocks)
         x_bar = project(constraints, node_blocks)
         d = x_bar - x_hat
-        alpha = max(g.T @ d / (d.T @ A @ d), 1)
+        divede = d.T @ A @ d
+        alpha = max(g.T @ d / (d.T @ A @ d), 1) if divede != 0 else 1
         x = x_hat + alpha * d
         node_blocks.positions = x
 
@@ -83,20 +86,21 @@ def project(constraints: Constraints, node_blocks: NodeBlocks):
     if len(C) == 0:
         return x
 
-    c = np.argmax([violation(ci) for ci in range(len(C))])
+    c = np.argmax([violation(ci, constraints, node_blocks) for ci in range(len(C))])
 
-    while violation(c) > 0:
-        c_left = constraints.left_index(c)
-        c_right = constraints.right_index(c)
+    while violation(c, constraints, node_blocks) > 0:
+        c_left = constraints.left(c)
+        c_right = constraints.right(c)
         if block[c_left] != block[c_right]:
             merge_blocks(block[c_left], block[c_right], c, constraints, node_blocks)
         else:
             expand_block(block[c_left], c, constraints, node_blocks)
-        c = np.argmax([violation(ci) for ci in range(len(C))])
+        c = np.argmax([violation(ci, constraints, node_blocks) for ci in range(len(C))])
+        print(f"{c=} {violation(c, constraints, node_blocks)=}")
 
     n = len(block)
     for i in range(n):
-        x[i] = B[block[i]].posn + offset[i]
+        x[i][0] = B[block[i]].posn + offset[i]
 
     node_blocks.positions = x
     return x
@@ -136,15 +140,15 @@ def expand_block(b, c_tilde, constraints: Constraints, node_blocks: NodeBlocks):
         lm[c] = 0
     AC: set = B[b].active
 
-    c_tilde_left = constraints.left_index(c_tilde)
-    c_tilde_right = constraints.right_index(c_tilde)
-    comp_dfdv(c_tilde_left, AC, None, constraints, x)
-    v = comp_path(c_tilde_left, c_tilde_right, AC)
+    c_tilde_left = constraints.left(c_tilde)
+    c_tilde_right = constraints.right(c_tilde)
+    comp_dfdv(c_tilde_left, AC, None, constraints, node_blocks)
+    v = comp_path(c_tilde_left, c_tilde_right, AC, constraints)
     ps = set()
     for c in AC:
+        c_left = constraints.left(c)
+        c_right = constraints.right(c)
         for j in range(len(v) - 1):
-            c_left = constraints.left_index(c)
-            c_right = constraints.right_index(c)
             if c_left == v[j] and c_right == v[j + 1]:
                 ps.add(c)
                 break
@@ -154,12 +158,11 @@ def expand_block(b, c_tilde, constraints: Constraints, node_blocks: NodeBlocks):
         sc = ps[np.argmin([lm[c] for c in ps])]
         AC.discard(sc)
 
-    # for v in connected(right(c_tilde), AC):
-    for v in B[c_tilde_right].vars:
-        offset[v] += violation(c_tilde)
+    for v in connected(c_tilde_right, AC, constraints):
+        offset[v] += violation(c_tilde, constraints, node_blocks)
     AC.add(c_tilde)
     B[b].active = AC
-    B[b].posn = sum([x[j] - offset[j] for j in B[b].vars]) / B[b].nvars
+    B[b].posn = sum([x[j][0] - offset[j] for j in B[b].vars]) / B[b].nvars
 
     node_blocks.positions = x
     node_blocks.B = B
@@ -170,21 +173,21 @@ def comp_dfdv(v, AC, u, constraints: Constraints, node_blocks: NodeBlocks):
     global lm
     x = node_blocks.positions
 
-    dfdv = node_blocks.posn(v) - x[v]
+    dfdv = node_blocks.posn(v) - x[v][0]
     if v == u:
         return dfdv
 
     for c in AC:
-        c_left = constraints.left_index(c)
-        c_right = constraints.right_index(c)
+        c_left = constraints.left(c)
+        c_right = constraints.right(c)
         if not (v == c_left and u != c_right):
             continue
-        lm[c] = comp_dfdv(c_right, AC, v, node_blocks)
+        lm[c] = comp_dfdv(c_right, AC, v, constraints, node_blocks)
         dfdv += lm[c]
     for c in AC:
         if not (v == c_right and u != c_left):
             continue
-        lm[c] = -comp_dfdv(c_left, AC, v, node_blocks)
+        lm[c] = -comp_dfdv(c_left, AC, v, constraints, node_blocks)
         dfdv -= lm[c]
 
     return dfdv
@@ -202,8 +205,7 @@ def split_blocks(constraints: Constraints, node_blocks: NodeBlocks):
         if B[i].nvars == 0:
             continue
 
-        B[i].posn = sum([x[j] - offset[j] for j in B[i].vars]) / B[i].nvars
-
+        B[i].posn = sum([x[j][0] - offset[j] for j in B[i].vars]) / B[i].nvars
         AC: set = B[i].active
         for c in AC:
             lm[c] = 0
@@ -220,7 +222,7 @@ def split_blocks(constraints: Constraints, node_blocks: NodeBlocks):
             break
         no_split = False
         AC.remove(sc)
-        s = constraints.right_index(sc)
+        s = constraints.right(sc)
         B[s].vars = connected(s, AC, constraints)
         for v in B[s].vars:
             block[v] = s
@@ -233,14 +235,18 @@ def split_blocks(constraints: Constraints, node_blocks: NodeBlocks):
         if B[s].nvars == 0:
             B[s].posn = 0
         else:
-            B[s].posn = sum([x[j] - offset[j] for j in B[s].vars]) / B[s].nvars
+            B[s].posn = sum([x[j][0] - offset[j] for j in B[s].vars]) / B[s].nvars
 
         if B[i].nvars == 0:
             B[i].posn = 0
         else:
-            B[i].posn = sum([x[j] - offset[j] for j in B[i].vars]) / B[i].nvars
+            B[i].posn = sum([x[j][0] - offset[j] for j in B[i].vars]) / B[i].nvars
 
-        B[i].active = {c for c in AC if left(c) in B[s].vars and right(c) in B[s].vars}
+        B[i].active = {
+            c
+            for c in AC
+            if constraints.left(c) in B[s].vars and constraints.right(c) in B[s].vars
+        }
         B[s].active = AC.difference(B[i].active)
 
     node_blocks.blocks = block
@@ -269,37 +275,17 @@ def connected(s, AC, constraints: Constraints):
 
 
 def comp_path(left, right, AC, constraints: Constraints):
-    c_graph = constraints.graph
-    v = set()
-    v.add(left)
-    v.add(right)
-    # left to right path nodes
-    stack = [left]
-    while len(stack) > 0:
-        u = stack.pop()
-        for vv, cost in c_graph[u]:
-            if u not in AC or v not in AC:
-                continue
-            if vv in v:
-                continue
-            v.add(vv)
-            stack.append(vv)
+    global G
+    path = list(nx.all_simple_paths(G, left, right))[0]
 
-    return list(v)
+    return path
 
 
 def stress_majorization(
     nodes, links, *, dim=2, initZ=None, constraints: Constraints = None
 ):
-    global x
-    global blocks
-    global offset
-    global B
-
+    global G
     n = len(nodes)
-
-    blocks = [i for i in range(n)]
-    offset = [0 for _ in range(n)]
 
     sigmas = [[0] * n] * n
     for i, j in links:
@@ -319,43 +305,48 @@ def stress_majorization(
         Z = initZ
     dim = len(Z[0])
     Z[0] = [0 for _ in range(dim)]
-    x = [[Z[i][1]] for i in range(n)]
-    B = [Block(i, Z[i][1]) for i in range(n)]
-    x_blocks = NodeBlocks([Z[i][1] for i in range(n)])
+    x_blocks = NodeBlocks(Z[:, 0])
+    y_blocks = NodeBlocks(Z[:, 1])
     #
     alpha = 2
     weights = weights_of_normalization_constant(alpha, dist)
 
     Lw = weight_laplacian(weights)
 
-    def update_ipset_cola():
+    def update_ipset_cola(blocks: NodeBlocks):
         # ipsep_cola
         Lz = z_laplacian(weights, dist, Z)
         b = (Lz @ Z[:, 1]).reshape(-1, 1)
         A = Lw
-        delta_x = solve_QPSC(A, b, constraints)
+        delta_x = solve_QPSC(A, b, constraints, blocks)
         Z[:, 1:2] = delta_x.flatten()[:, None]
         Z[0] = [0 for _ in range(dim)]
+        blocks.positions = Z[:, 1].reshape(-1, 1)
+        return blocks
 
-    update_ipset_cola()
+    # update_ipset_cola(y_blocks)
 
     # 終了する閾値
-    eps = 0.000_01
+    eps = 0.0001
     now_stress = stress(Z, dist, weights)
     new_stress = 0.5 * now_stress
 
     def delta_stress(now, new):
-        return (now - new) / now
+        return abs(now - new) / now
 
     while True:
         Lz = z_laplacian(weights, dist, Z)
 
-        # for a in range(dim):
-        #     # Ax = b
-        #     Z[1:, a] = cg(Lw[1:, 1:], (Lz @ Z[:, a])[1:])[0]
-        Z[1:, 0] = cg(Lw[1:, 1:], (Lz @ Z[:, 0])[1:])[0]
+        for a in range(dim):
+            # Ax = b
+            Z[1:, a] = cg(Lw[1:, 1:], (Lz @ Z[:, a])[1:])[0]
+        # Z[1:, 0] = cg(Lw[1:, 1:], (Lz @ Z[:, 0])[1:])[0]
 
-        update_ipset_cola()
+        # x_blocks = update_ipset_cola(Z, x_blocks)
+
+        y_blocks = update_ipset_cola(y_blocks)
+
+        # Z = np.concatenate([x_blocks.positions, y_blocks.positions], axis=1)
 
         new_stress = stress(Z, dist, weights)
         print(f"{now_stress=} -> {new_stress=}")
@@ -364,23 +355,26 @@ def stress_majorization(
         if delta_stress(now_stress, new_stress) < eps:
             break
         now_stress = new_stress
+        break
 
     return Z
 
 
 if __name__ == "__main__":
-    with open("./src/majorization/data.json") as f:
+    # with open("./src/ipsep_cola/line.json") as f:
+    #     data = json.load(f)
+    with open("./src/ipsep_cola/line.json") as f:
         data = json.load(f)
 
-    nodes = [i + 1 for i in range(len(data["nodes"]))]
+    nodes = [i for i in range(len(data["nodes"]))]
     n = len(nodes)
 
-    links = [[d["source"] + 1, d["target"] + 1] for d in data["links"]]
+    links = [[d["source"], d["target"]] for d in data["links"]]
     constraints = data["constraints"]
 
     C = []
-    # for c in constraints:
-    #     C.append([c["right"], c["left"], 1])
+    for c in constraints:
+        C.append([c["left"], c["right"], 1])
     const = Constraints(C, n)
 
     Z = stress_majorization(nodes, links, constraints=const)
@@ -391,14 +385,15 @@ if __name__ == "__main__":
             G.add_node(node)
         for link in links:
             G.add_edge(*link)
-        position = {i + 1: Z[i] for i in range(n)}
+
+        position = {i: Z[i] for i in range(n)}
 
         today = datetime.date.today()
         now = datetime.datetime.now().time()
         os.makedirs(f"result/{today}", exist_ok=True)
 
         plt.figure(figsize=(10, 10))
-        nx.draw(G, pos=position, node_size=300, labels={i + 1: i for i in range(n)})
+        nx.draw(G, pos=position, node_size=300, labels={i: i for i in range(n)})
         plt.savefig(f"result/{today}/{now}.png")
         # plt.show()
 
