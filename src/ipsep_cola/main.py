3e81c4c6432e4a7aea4f272f5fa77d48e2d53755
itochs <halglobe0108@gmail.com>
2024-04-22 01:16:27 +0900

    gapはノードサイズ以上の値じゃないと重なる

--- src/ipsep_cola/main.py
+++ src/ipsep_cola/main.py
@@ -20,13 +20,14 @@ from scipy.sparse.linalg import cg
 lm = dict()
 
 
-def IPSep_CoLa(graph: nx.Graph, constraints: Constraints, edge_length: float = 20.0):
+def IPSep_CoLa(graph: nx.Graph, C: dict[str, list], edge_length: float = 20.0):
     """
     graph: networkx.Graph
     graph.edges: [(i, j), ...] node index
     """
 
     n = len(graph.nodes)
+
     sigmas = [[0] * n] * n
     for i, j in graph.edges:
         sigmas[i][j] = 1
@@ -49,31 +50,26 @@ def IPSep_CoLa(graph: nx.Graph, constraints: Constraints, edge_length: float = 2
     def delta_stress(now, new):
         return abs(now - new) / now
 
-    y_blocks = NodeBlocks(Z[:, 1].flatten())
-
     iter = 100
     while delta_stress(now_stress, new_stress) > eps and iter > 0:
         iter -= 1
         Lz = z_laplacian(weight, dist, Z)
         for a in range(2):
             Z[1:, a] = cg(Lw[1:, 1:], (Lz @ Z[:, a])[1:])[0]
-        # Z[1:, 0] = cg(Lw[1:, 1:], (Lz @ Z[:, 0])[1:])[0]
 
-        y_blocks = NodeBlocks(Z[:, 1].flatten())
-        b = (Lz @ Z[:, 1]).reshape(-1, 1)
-        A = Lw
-        delta_x = solve_QPSC(A, b, constraints, y_blocks)
-        # print(delta_x)
-        Z[:, 1:2] = delta_x.flatten()[:, None]
+            blocks = NodeBlocks(Z[:, a].flatten())
+            b = (Lz @ Z[:, a]).reshape(-1, 1)
+            A = Lw
+            constraints = Constraints(C["x" if a == 0 else "y"], n)
+            delta_x = solve_QPSC(A, b, constraints, blocks)
+            Z[:, a : a + 1] = delta_x.flatten()[:, None]
+
         for i in range(n - 1, -1, -1):
             Z[i] -= Z[0]
 
         now_stress = new_stress
         new_stress = stress(Z, dist, weight)
         print("stress", now_stress, "->", new_stress)
-
-    # json.dump(y_blocks.__dict__(), open("result.json", "w"))
-    # print(y_blocks.__dict__())
 
     return Z
 
@@ -116,21 +112,46 @@ if __name__ == "__main__":
         plt.title(file_name)
         nx.draw(G, pos=position, node_size=300, labels={i: i for i in range(n)}, ax=ax)
         plt.savefig(f"{dir}/{file_name}.png")
+        plt.close()
 
-    # C = []
-    # for c in constraints:
-    #     C.append([c["left"], c["right"], 30])
-    # const = Constraints(C, n)
-    # Z = IPSep_CoLa(G, const, edge_length=20.0)
-    # view(Z, "IPSep_Cola (seed 0) gap=30, edge_length=20.0.png")
+    edge_length = 20.0
+    dist = floyd_warshall_numpy(G)
+    # Tips: gapは実際のノードサイズ以上にしてくれとのこと.
 
-    gaps = np.arange(10, 51, 10)
-    edge_lengths = np.arange(10.0, 51.0, 10.0)
-    for gap in gaps:
-        for edge_length in edge_lengths:
-            C = []
-            for c in constraints:
-                C.append([c["left"], c["right"], gap])
-            const = Constraints(C, n)
-            Z = IPSep_CoLa(G, const, edge_length=edge_length)
-            view(Z, f"IPSep_Cola (seed 0) {gap=}, {edge_length=}.png")
+    def once():
+        C: dict[str, list] = dict()
+        C["y"] = []
+        C["x"] = []
+        for c in constraints:
+            left = c["left"]
+            right = c["right"]
+            axis = c["axis"]
+            C[axis].append([left, right, max(dist[left][right] * edge_length, 30)])
+        Z = IPSep_CoLa(G, C, edge_length=20.0)
+        view(Z, "IPSep_Cola (seed 0) gap=30, edge_length=20.0")
+
+    def mutiple():
+        gaps = np.arange(10, 51, 10)
+        edge_lengths = np.arange(10.0, 51.0, 10.0)
+        for gap in gaps:
+            for edge_length in edge_lengths:
+                C: dict[str, list] = dict()
+                C["y"] = []
+                C["x"] = []
+                for c in constraints:
+                    left = c["left"]
+                    right = c["right"]
+                    axis = c["axis"]
+                    C[axis].append([left, right, gap])
+
+                try:
+                    Z = IPSep_CoLa(G, C, edge_length=edge_length)
+                    view(
+                        Z,
+                        f"IPSep_Cola (seed 0) base{gap}, {edge_length=}.png",
+                    )
+                except Exception as e:
+                    print(e)
+
+    # once()
+    mutiple()
