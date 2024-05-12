5cafeb1b4f84b67c0142e59da82b5f25463ab04c
itochs <halglobe0108@gmail.com>
2024-05-12 17:41:09 +0900

    change data

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -1,45 +1,82 @@
 import * as cola from "webcola";
 import * as d3 from "d3";
-import graph from "../../src/data/no_cycle_tree.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [graph, setGraph] = useState();
+
+  useEffect(() => {
+    fetch("/no_cycle_tree.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setGraph(data));
+  }, []);
+
+  const handleSelectChange = (e) => {
+    const filePath = `/${e.target.value}.json`;
+    fetch(filePath, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setGraph(data));
+  };
+
   const d3cola = cola.d3adaptor(d3);
   const width = 960;
   const height = 600;
-  d3cola
-    .nodes(graph.nodes)
-    .links(graph.links)
-    .constraints(graph.constraints)
-    .size([width, height])
-    .start(10, 15, 20);
+  if (graph) {
+    d3cola
+      .nodes(graph?.nodes)
+      .links(graph?.links)
+      .constraints(graph?.constraints)
+      .size([width, height])
+      .start(10, 15, 20);
+  }
 
-  const nodes = graph.nodes;
-  const links = graph.links;
+  const nodes = graph?.nodes;
+  const links = graph?.links;
 
   return (
     <>
-      <svg width="960" height="600">
-        {nodes.map((node) => (
-          <circle
-            key={node.name}
-            cx={node.x}
-            cy={node.y}
-            r="5"
-            fill="blue"
-            stroke="black"
-          />
-        ))}
-        {links.map((link) => (
-          <line
-            key={link.source.index + "-" + link.target.index}
-            x1={link.source.x}
-            y1={link.source.y}
-            x2={link.target.x}
-            y2={link.target.y}
-            stroke="black"
-          />
-        ))}
-      </svg>
+      <select onChange={handleSelectChange} defaultValue={"no_cycle_tree"}>
+        <option value="no_cycle_tree">tree</option>
+        <option value="w">w</option>
+      </select>
+      {graph && (
+        <svg width="960" height="600">
+          <g>
+            {nodes.map((node) => (
+              <circle
+                key={node.name}
+                cx={node.x}
+                cy={node.y}
+                r="5"
+                fill="blue"
+                stroke="black"
+              />
+            ))}
+          </g>
+          <g>
+            {links.map((link) => (
+              <line
+                key={link.source.index + "-" + link.target.index}
+                x1={link.source.x}
+                y1={link.source.y}
+                x2={link.target.x}
+                y2={link.target.y}
+                stroke="black"
+              />
+            ))}
+          </g>
+        </svg>
+      )}
     </>
   );
 }
