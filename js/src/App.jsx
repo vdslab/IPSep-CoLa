99d9b4a912be0fd40f09b94d8d1c4c69739ff3df
itochs <halglobe0108@gmail.com>
2024-05-12 16:06:18 +0900

    add d3 cola

--- js/src/App.jsx
+++ js/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import * as cola from "webcola";
+import * as d3 from "d3";
+import graph from "../../src/data/no_cycle_tree.json";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const d3cola = cola.d3adaptor(d3);
+  const width = 960;
+  const height = 600;
+  d3cola
+    .nodes(graph.nodes)
+    .links(graph.links)
+    .constraints(graph.constraints)
+    .size([width, height])
+    .start(10, 15, 20);
+
+  const nodes = graph.nodes;
+  const links = graph.links;
 
   return (
     <>
@@ -28,8 +44,30 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <svg width="960" height="600">
+        {nodes.map((node) => (
+          <circle
+            key={node.name}
+            cx={node.x}
+            cy={node.y}
+            r="5"
+            fill="blue"
+            stroke="black"
+          />
+        ))}
+        {links.map((link) => (
+          <line
+            key={link.source.index + "-" + link.target.index}
+            x1={link.source.x}
+            y1={link.source.y}
+            x2={link.target.x}
+            y2={link.target.y}
+            stroke="black"
+          />
+        ))}
+      </svg>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
