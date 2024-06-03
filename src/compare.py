d5066fbad06f78bfdddde183383970316f357194
itochs <halglobe0108@gmail.com>
2024-06-03 14:01:16 +0900

    比較

--- src/compare.py
+++ src/compare.py
@@ -1,103 +1,87 @@
 import datetime
 import os
-from sgd.main import plot_sgd
-
 import matplotlib.pyplot as plt
-import networkx as nx
-import numpy as np
-import json
-from majorization.main import stress, weights_of_normalization_constant
-from networkx import floyd_warshall_numpy
-from ipsep_cola.main import IPSep_CoLa
-import time
+from ipsep_cola.main import run_IPSep_CoLa
 
-today = datetime.date.today()
-now = datetime.datetime.now()
-save_dir = f"result/SGD/edge_gap/{today}/{now}"
-os.makedirs(save_dir, exist_ok=True)
-
-# for gap in gaps:
-#     for edge_length in edge_lengths:
-filename = "les_miserables_graph.json"
-filename = "./src/data/no_cycle_tree.json"
+from sgd.main import sgd_with_project
+from util.graph import get_graph_and_constraints, plot_graph
 
 
-start = time.time()
-sgd_stresses, sgd_times = plot_sgd(filename, save_dir, 20, 30)
-sgd_times = [s - start for s in sgd_times]
+def main():
+    today = datetime.date.today()
+    now = datetime.datetime.now().time()
+    save_dir = f"result/compare/{today}/{now}"
+    os.makedirs(save_dir, exist_ok=True)
 
-with open(filename) as f:
-    data = json.load(f)
+    # for gap in gaps:
+    #     for edge_length in edge_lengths:
+    filename = "les_miserables_graph.json"
+    # filename = "./src/data/no_cycle_tree.json"
 
-nodes = [i for i in range(len(data["nodes"]))]
-n = len(nodes)
+    graph, _ = get_graph_and_constraints(filename)
 
-links = [[d["source"], d["target"]] for d in data["links"]]
-constraints = data["constraints"]
+    print("do sgd with project")
+    Z, sgd_stresses, sgd_times = sgd_with_project(
+        file_path=filename,
+        edge_length=20,
+        gap=30,
+        iter_count=300,
+        eps=0.01,
+    )
 
-G = nx.Graph()
-for node in nodes:
-    G.add_node(node)
-for link in links:
-    G.add_edge(*link)
+    graph, _ = get_graph_and_constraints(filename)
+    plot_graph(graph, Z, save_dir, "sgd_with_project.png")
 
-np.random.seed(0)
-today = datetime.date.today()
-now = datetime.datetime.now().time()
-dir = f"result/IPSep_Cola/{today}/{now.hour}-{now.minute}-{now.second}"
+    print("do IPSep CoLa")
+    Z, stresses, times = run_IPSep_CoLa(
+        file_path=filename,
+        edge_length=20,
+        gap=30,
+        unconstrainedIter=100,
+        customConstrainedIter=100,
+        allIter=100,
+    )
+
+    plot_graph(graph, Z, save_dir, "IPSep_CoLa.png")
+
+    def save_fig(*, xy, xlabel, ylabel, title, legend, filename, save_dir=""):
+        fig, ax = plt.subplots()
+        for x, y in xy:
+            ax.plot(x, y)
+        ax.set_yscale("log")
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel(ylabel)
+        ax.set_title(title)
+        ax.legend(legend)
+        plt.savefig(os.path.join(save_dir, filename))
+        plt.close()
+
+    save_fig(
+        xy=[(times, stresses), (sgd_times, sgd_stresses)],
+        xlabel="time (second)",
+        ylabel="stress",
+        title="stress-time",
+        legend=["IPSep_CoLa", "use SGD"],
+        filename="stress_time.png",
+        save_dir=save_dir,
+    )
+
+    save_fig(
+        xy=[
+            (list(range(len(stresses))), stresses),
+            (list(range(len(sgd_stresses))), sgd_stresses),
+        ],
+        xlabel="iteration",
+        ylabel="stress",
+        title="stress-iteration",
+        legend=["IPSep_CoLa", "use SGD"],
+        filename="stress.png",
+        save_dir=save_dir,
+    )
 
 
-def view(position, file_name):
-    DG = nx.DiGraph()
-    for node in nodes:
-        DG.add_node(node)
-    for link in links:
-        DG.add_edge(*link)
+# import cProfile
 
-    position = {i: position[i] for i in range(n)}
-
-    os.makedirs(dir, exist_ok=True)
-
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.set_aspect("equal")
-
-    plt.title(file_name)
-    nx.draw(G, pos=position, node_size=300, labels={i: i for i in range(n)}, ax=ax)
-    plt.savefig(f"{save_dir}/{file_name}.png")
-    plt.close()
-
-
-dist = floyd_warshall_numpy(G)
-# Tips: gapは実際のノードサイズ以上にしてくれとのこと.
-
-
-C: dict[str, list] = dict()
-C["y"] = []
-C["x"] = []
-for c in constraints:
-    left = c["left"]
-    right = c["right"]
-    axis = c["axis"]
-    gap = c["gap"]
-    C[axis].append([left, right, gap])
-start = time.time()
-Z, stresses, times = IPSep_CoLa(G, C, edge_length=20.0)
-times = [s - start for s in times]
-weight = weights_of_normalization_constant(2, dist)
-stress(Z, dist, weight)
-print(stresses)
-
-view(Z, "IPSep_Cola (seed 0) edge_length=20.0")
-
-fig, ax = plt.subplots()
-ax.plot(times, stresses)
-ax.plot(sgd_times, sgd_stresses)
-ax.set_yscale("log")
-ax.set_xlabel("time (second)")
-ax.set_ylabel("stress")
-ax.legend(["IPSep_CoLa", "use SGD"])
-plt.savefig(f"{save_dir}/stress_time.png")
-plt.close()
-print(sgd_times)
-print(times)
-print(time.time())
+if __name__ == "__main__":
+    # cProfile.run("main()", sort="cumtime", filename="compare.prof")
+    main()
