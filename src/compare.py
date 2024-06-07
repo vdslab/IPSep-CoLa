1411614f9a4904cb29d983284bf05661997fe59d
itochs <halglobe0108@gmail.com>
2024-06-07 11:23:20 +0900

    save fig: stress-iter, stress-time

--- src/compare.py
+++ src/compare.py
@@ -15,69 +15,73 @@ def main():
 
     # for gap in gaps:
     #     for edge_length in edge_lengths:
-    filename = "les_miserables_graph.json"
-    # filename = "./src/data/no_cycle_tree.json"
+    filenames = [
+        "les_miserables_graph.json",
+        "florentine_families_graph.json",
+        "davis_southern_women_graph.json",
+        "karate_club_graph.json",
+    ]
+    for filename in filenames:
+        graph, _ = get_graph_and_constraints(filename)
 
-    graph, _ = get_graph_and_constraints(filename)
+        print("do sgd with project")
+        Z, sgd_stresses, sgd_times = sgd_with_project(
+            file_path=filename,
+            edge_length=20,
+            gap=30,
+            iter_count=30,
+            eps=0.01,
+        )
 
-    print("do sgd with project")
-    Z, sgd_stresses, sgd_times = sgd_with_project(
-        file_path=filename,
-        edge_length=20,
-        gap=30,
-        iter_count=300,
-        eps=0.01,
-    )
+        graph, _ = get_graph_and_constraints(filename)
+        plot_graph(graph, Z, save_dir, f"{filename}_sgd_with_project.png")
 
-    graph, _ = get_graph_and_constraints(filename)
-    plot_graph(graph, Z, save_dir, "sgd_with_project.png")
+        print("do IPSep CoLa")
+        Z, stresses, times = run_IPSep_CoLa(
+            file_path=filename,
+            edge_length=20,
+            gap=30,
+            unconstrainedIter=10,
+            customConstrainedIter=10,
+            allIter=10,
+        )
 
-    print("do IPSep CoLa")
-    Z, stresses, times = run_IPSep_CoLa(
-        file_path=filename,
-        edge_length=20,
-        gap=30,
-        unconstrainedIter=100,
-        customConstrainedIter=100,
-        allIter=100,
-    )
+        plot_graph(graph, Z, save_dir, f"{filename}_IPSep_CoLa.png")
 
-    plot_graph(graph, Z, save_dir, "IPSep_CoLa.png")
+        def save_fig(*, xy, xlabel, ylabel, title, legend, filename, save_dir=""):
+            fig, ax = plt.subplots()
+            for x, y in xy:
+                ax.plot(x, y)
+            ax.set_yscale("log")
+            ax.set_xlabel(xlabel)
+            ax.set_ylabel(ylabel)
+            ax.set_title(title)
+            ax.legend(legend)
+            plt.savefig(os.path.join(save_dir, filename))
+            plt.close()
 
-    def save_fig(*, xy, xlabel, ylabel, title, legend, filename, save_dir=""):
-        fig, ax = plt.subplots()
-        for x, y in xy:
-            ax.plot(x, y)
-        ax.set_yscale("log")
-        ax.set_xlabel(xlabel)
-        ax.set_ylabel(ylabel)
-        ax.set_title(title)
-        ax.legend(legend)
-        plt.savefig(os.path.join(save_dir, filename))
-        plt.close()
+        save_fig(
+            xy=[(times, stresses), (sgd_times, sgd_stresses)],
+            xlabel="time (second)",
+            ylabel="stress",
+            title="stress-time",
+            legend=["IPSep_CoLa", "use SGD"],
+            filename=f"{filename}_tress_time.png",
+            save_dir=save_dir,
+        )
 
-    save_fig(
-        xy=[(times, stresses), (sgd_times, sgd_stresses)],
-        xlabel="time (second)",
-        ylabel="stress",
-        title="stress-time",
-        legend=["IPSep_CoLa", "use SGD"],
-        filename="stress_time.png",
-        save_dir=save_dir,
-    )
-
-    save_fig(
-        xy=[
-            (list(range(len(stresses))), stresses),
-            (list(range(len(sgd_stresses))), sgd_stresses),
-        ],
-        xlabel="iteration",
-        ylabel="stress",
-        title="stress-iteration",
-        legend=["IPSep_CoLa", "use SGD"],
-        filename="stress.png",
-        save_dir=save_dir,
-    )
+        save_fig(
+            xy=[
+                (list(range(len(stresses))), stresses),
+                (list(range(len(sgd_stresses))), sgd_stresses),
+            ],
+            xlabel="iteration",
+            ylabel="stress",
+            title="stress-iteration",
+            legend=["IPSep_CoLa", "use SGD"],
+            filename=f"{filename}_stress.png",
+            save_dir=save_dir,
+        )
 
 
 # import cProfile
