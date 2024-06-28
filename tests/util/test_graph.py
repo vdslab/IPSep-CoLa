91d841da38fcf64f08fbae09b4b4a6784df09ff9
itochs <halglobe0108@gmail.com>
2024-06-28 14:53:19 +0900

    テスト

--- tests/util/test_graph.py
+++ tests/util/test_graph.py
@@ -1,7 +1,8 @@
 import unittest
 
 import numpy as np
-from util.graph import stress
+from networkx import Graph
+from util.graph import distance_matrix, stress
 
 
 class TestGraph(unittest.TestCase):
@@ -20,6 +21,18 @@ class TestGraph(unittest.TestCase):
         Ystress = stress(Y, Ydist, Yweight)
         exact = 31 - 8 * pow(2, 0.5) - 4 * pow(5, 0.5)
         self.assertLess(Ystress - exact, 1e-4)
+
+    def test_distance_matrix(self):
+        graph = Graph()
+        graph.add_edge(1, 2)
+        graph.add_edge(2, 3)
+        matrix = distance_matrix(graph)
+        exact = [[0, 1, 2], [1, 0, 1], [2, 1, 0]]
+        self.assertEqual(len(matrix), len(exact))
+        for i in range(len(matrix)):
+            self.assertEqual(len(matrix[i]), len(exact[i]))
+            for j in range(len(matrix[i])):
+                self.assertAlmostEqual(matrix[i][j], exact[i][j], 1e-4)
 
 
 if __name__ == "__main__":
