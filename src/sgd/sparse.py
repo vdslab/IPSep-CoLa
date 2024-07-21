b4e06457b9a97ba51fb76c438ad7095dfaf3fc16
itochs <halglobe0108@gmail.com>
2024-07-21 22:00:10 +0900

    full sgd

--- src/sgd/sparse.py
+++ src/sgd/sparse.py
@@ -28,7 +28,9 @@ logger.debug("hello")
 dtoday = datetime.date.today()
 now = datetime.datetime.now().time()
 save_dir = f"./result/SGD/sparse/{dtoday}/{now}"
+fullsgd_save_dir = "./src/data/SGD/full/"
 os.makedirs(save_dir, exist_ok=True)
+os.makedirs(fullsgd_save_dir, exist_ok=True)
 
 
 def nxgraph_to_eggraph(graph: nx.Graph) -> tuple[eg.Graph, dict]:
@@ -79,22 +81,24 @@ def sparse_sgd_with_constraints(
 
     stresses: dict[list] = {}
     for _ in range(50):
-        # for i in range(20, 51, 5):
-        for i in range(10, 101, 10):
+        for it in range(10, 61, 5):
+            iitt = 10
             scheduler = sgd.scheduler(
-                100,  # number of iterations
+                iitt,  # number of iterations
                 eps,  # eps: eta_min = eps * min d[i, j] ^ 2
             )
             def step(eta):
                 sgd.shuffle(rng)
                 sgd.apply(drawing, eta)
-            scheduler.run(step)
+            for _ in range(iitt):
+                scheduler.step(step)
 
             eta_max = 1 / (d_max ** 2)
             eta_min = eps / (d_min ** 2)
-            b = 1 / (i - 1) * np.log(eta_min / eta_max)
+            b = 1 / (it - 1) * np.log(eta_min / eta_max)
 
             def apply_project(j):
+                nonlocal it
                 _y = np.array([drawing.y(i) for i in range(n)])
                 # y_hat = _y.copy()
                 # y_hat = y_hat.reshape(-1, 1)
@@ -109,31 +113,22 @@ def sparse_sgd_with_constraints(
                 y = y.flatten()
 
                 eta = eta_max * np.exp(b * j)
-                # print(eta)
+                # print((eta-eta_min)/(eta_max-eta_min))
                 d = y - _y
-                y = _y + d*(eta/eta_max)
+                y = _y + d*(1+(eta-eta_min)/(eta_max-eta_min))
                 
                 for i in range(n):
                     drawing.set_y(i, y[i])
 
             scheduler = sgd.scheduler(
-                i,  # number of iterations
+                it,  # number of iterations
                 eps,  # eps: eta_min = eps * min d[i, j] ^ 2
             )
-
-            # j = 100 - i
-            # # s = []
-            # for _ in range(i):
-            #     scheduler.step(step)
-            # for i in range(15):
-            #     apply_project()
-            for _ in range(i):
+            for j in range(it):
                 scheduler.step(step)
-                apply_project(i)
-                # apply_project(1)
-                # s.append()
-            # print(stresses)
-            stresses.setdefault((100, i), []).append(eg.stress(drawing, d))
+                apply_project(j)
+
+            stresses.setdefault((iitt, it), []).append(eg.stress(drawing, d))
 
     fig = plt.figure(figsize=(20, 15), facecolor="lightblue")
     boxitem_st = []
@@ -143,12 +138,19 @@ def sparse_sgd_with_constraints(
             continue
         boxitem_st.append(st)
         boxitem_la.append(f"SGD{ij[0]}_project{ij[1]}")
+    
+    with open("src/data/cola/stress/no_cycle_tree.json") as f:
+        s = json.load(f)
+        boxitem_st.append(s)
+        boxitem_la.append("webcola stress")
+
 
     plt.boxplot(boxitem_st, labels=boxitem_la)
     plt.title("SGD_project_stress")
     plt.ylabel("stress")
     plt.legend()
     # plt.xlim(right=150)
+    # plt.ylim(270, 300)
     # plt.axvline(i - 1, 0, 1, color="g", alpha=0.5, label="finish SGD", linestyle=":")
     plt.savefig(f"{save_dir}/SGD_project_{name}.png")
     plt.close()
@@ -157,16 +159,147 @@ def sparse_sgd_with_constraints(
     # print(eg.stress(drawing, d))
     return pos, drawing
 
+def full_sgd_with_constraints(
+    graph: eg.Graph,
+    indices: dict,
+    constraints: Constraints,
+    *,
+    edge_length: int,
+    sgd_iter: int,
+    project_iter: int,
+    eps: float,
+    seed: int = None,
+) -> tuple[list[tuple[int, int]], float]:
+    """
+    # Returns
+    pos: list[tuple[int, int]]
+        - x, y
+
+    stress: float
+    """
+    n = len(indices)
+
+    drawing = eg.DrawingEuclidean2d.initial_placement(graph)
+    rng = eg.Rng.seed_from(seed) if seed is not None else eg.Rng()
+    d = eg.all_sources_dijkstra(graph, lambda _: edge_length)
+    ds = np.array([[d.get(i, j) for j in range(n)] for i in range(n)])
+    ds = ds.flatten()
+    ds = ds[ds != 0]
+    d_min = np.min(ds)
+    d_max = np.max(ds)
+    sgd = eg.FullSgd.new_with_distance_matrix(d)
+
+    scheduler = sgd.scheduler(
+        sgd_iter,  # number of iterations
+        eps,  # eps: eta_min = eps * min d[i, j] ^ 2
+    )
+    def step(eta):
+        sgd.shuffle(rng)
+        sgd.apply(drawing, eta)
+    scheduler.run(step)
+
+    eta_max = 1 / (d_max ** 2)
+    eta_min = eps / (d_min ** 2)
+    b = 1 / (project_iter - 1) * np.log(eta_min / eta_max)
+
+    def apply_project(j):
+        _y = np.array([drawing.y(i) for i in range(n)])
+
+        blocks = NodeBlocks(_y)
+        y = project(constraints, blocks)
+        y = y.flatten()
+
+        eta = eta_max * np.exp(b * j)
+        d = y - _y
+        y = _y + d*(1+(eta-eta_min)/(eta_max-eta_min))
+        
+        for i in range(n):
+            drawing.set_y(i, y[i])
+
+    scheduler = sgd.scheduler(
+        project_iter,  # number of iterations
+        eps,  # eps: eta_min = eps * min d[i, j] ^ 2
+    )
+    for j in range(project_iter):
+        scheduler.step(step)
+        apply_project(j)
+
+    pos = [(drawing.x(i), drawing.y(i)) for u, i in indices.items()]
+    # print(eg.stress(drawing, d))
+    return pos, drawing
+
+
+def full_sgd_main():
+    files = [
+        "./src/data/json/download/no_cycle_tree.json",
+        "./src/data/json/download/qh882.json",
+        "./src/data/json/download/dwt_1005.json",
+        "./src/data/json/download/1138_bus.json",
+        "./src/data/json/download/USpowerGrid.json",
+        "./src/data/json/download/dwt_2680.json",
+        "./src/data/json/download/3elt.json",
+    ]
+    for file in files:
+        basename = os.path.basename(file)
+        print(basename)
+        title = os.path.splitext(basename)[0]
+
+        stresses = []
+        stressfilepath = os.path.join(fullsgd_save_dir,basename)
+        imagepath = os.path.join(fullsgd_save_dir, title)
+        try:
+            with open(stressfilepath, "r") as f:
+                stresses = json.load(f)
+        except Exception as e:
+            print(e)
+            print(file)
+
+        if len(stresses) >= 20:
+            continue
+
+        nx_graph, constraints_data = get_graph_and_constraints(file)
+        length = None
+        with open(f"./src/data/json/dist/{basename}") as f:
+            json_data = json.load(f)
+            length = json_data["length"]
+
+        C = get_constraints_dict(constraints_data, default_gap=20)
+        constraints = Constraints(C["y"], nx_graph.number_of_nodes())
+        eggraph, indices = nxgraph_to_eggraph(nx_graph)
+        print("\t start dijkstra")
+        d = eg.all_sources_dijkstra(eggraph, lambda _: length)
+
+        for i in range(20):
+            if len(stresses) >= 20:
+                break
+
+            print("\t", i)
+            pos, drawing = full_sgd_with_constraints(
+                eggraph,
+                indices,
+                constraints,
+                edge_length=length,
+                sgd_iter=10,
+                project_iter=30,
+                eps=0.1,
+            )
+
+            stresses.append(eg.stress(drawing, d))
+            os.makedirs(imagepath, exist_ok=True)
+            plot_graph(nx_graph, pos, imagepath, f"{i}.png")
+            with open(stressfilepath, "w") as f:
+                json.dump(stresses, f, indent=2)
+        print(stresses)
 
 def main():
     files = [
         "./src/data/json/download/no_cycle_tree.json",
-        # "./src/data/json/download/qh882.json",
-        # "./src/data/json/download/dwt_1005.json",
-        # "./src/data/json/download/1138_bus.json",
-        # "./src/data/json/download/dwt_2680.json",
-        # "./src/data/json/download/USpowerGrid.json",
-        # "./src/data/json/download/3elt.json",
+        "./src/data/json/download/qh882.json",
+        "./src/data/json/download/dwt_1005.json",
+        "./src/data/json/download/1138_bus.json",
+        "./src/data/json/download/USpowerGrid.json",
+        "./src/data/json/download/dwt_2680.json",
+        "./src/data/json/download/3elt.json",
     ]
 
     for file in files:
@@ -221,4 +354,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    full_sgd_main()
