fe3ff3ef5b69dc9b660f7db60ed5505697f0d2b1
Yosuke Onoue <onoue@likr-lab.com>
2024-12-11 23:48:18 +0900

    レイアウト結果のプロット

---
+++ scripts/plot.py
@@ -0,0 +1,24 @@
+import argparse
+import json
+import os
+
+import networkx as nx
+
+from util.graph.plot import plot_graph
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('graph_file')
+    parser.add_argument('drawing_file')
+    parser.add_argument('output')
+    args = parser.parse_args()
+
+    graph = nx.node_link_graph(json.load(open(args.graph_file)))
+    pos = json.load(open(args.drawing_file))
+    os.makedirs(os.path.dirname(args.output), exist_ok=True)
+    plot_graph(graph, pos, args.output)
+
+
+if __name__ == '__main__':
+    main()
--- src/util/graph/plot.py
+++ src/util/graph/plot.py
@@ -4,21 +4,17 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def plot_graph(G: nx.Graph, Z, save_dir, file_name="graph.png", aspect="equal"):
-    n = G.number_of_nodes()
-    print(n)
-    position = {i: Z[i] for i in range(n)}
-
+def plot_graph(G: nx.Graph, pos, file_name="graph.png", aspect="equal"):
     fig, ax = plt.subplots(figsize=(10, 10))
     ax.set_aspect(aspect)
     nx.draw(
         G,
         with_labels=True,
-        pos=position,
+        pos=pos,
         node_shape="o",
         node_size=50,
         ax=ax,
     )
     plt.gca().invert_yaxis()
-    plt.savefig(os.path.join(save_dir, file_name))
+    plt.savefig(file_name)
     plt.close()
