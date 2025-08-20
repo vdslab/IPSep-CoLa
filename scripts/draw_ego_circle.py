e809b26d006dcf615adcb7daa947ac52961c8679
itochs <halglobe0108@gmail.com>
2025-08-20 21:56:45 +0900

    networkxでデータを持つとメモリが足りなかった

--- scripts/draw_ego_circle.py
+++ scripts/draw_ego_circle.py
@@ -5,7 +5,7 @@ import os
 import egraph as eg
 import networkx as nx
 
-from sgd.full import sgd
+from sgd.full_ego import sgd
 from util.graph import nxgraph_to_egDiGraph
 
 
@@ -25,51 +25,55 @@ def main():
     os.makedirs(args.dest, exist_ok=True)
     for filepath in args.input:
         basename = os.path.basename(filepath)
-        # TODO: ソーシャルグラフでの円形制約
-        # TODO: e-graphでのoverlap removal(OverwrapRemoval)
         graph: nx.Graph = nx.node_link_graph(json.load(open(filepath)), link="links")
-        degree = [(node, len(list(graph.neighbors(node)))) for node in graph.nodes]
-        degree.sort(key=lambda x: -x[1])
-        egos = [v for v, _ in degree[: args.center_nodes_count]]
-        # egos = ["0", "107", "348", "414", "686", "698", "1684", "1912", "3437", "3980"]
+        egos = graph.graph["egos"]
+
         eggraph, indices = nxgraph_to_egDiGraph(graph, egos)
         vi = {v: k for k, v in indices.items()}
+
         eg.remove_cycle(eggraph)
         longest = eg.LongestPath()
         nodeidx_layer = longest.assign_layers(eggraph)
-        print({vi[k]: v for k, v in nodeidx_layer.items()})
         max_layer = max(nodeidx_layer.values())
         layer_nodeidx = dict()
         for k, v in nodeidx_layer.items():
             layer_nodeidx.setdefault(v, []).append(k)
+        # print({vi[k]: v for k, v in nodeidx_layer.items()})
 
-        circle_constraint = []
-        for i in range(max_layer + 1):
-            circle_constraint.append(
-                {
-                    "type": "circle",
-                    "nodes": [vi[j] for j in layer_nodeidx[i]],
-                    "r": i + (1 if len(egos) != 1 else 0),
-                    "center": None if len(egos) != 1 else degree[0][0],
-                }
+        if graph.graph.get("constraints") is None:
+            circle_constraint = []
+            for i in range(max_layer + 1):
+                circle_constraint.append(
+                    {
+                        "type": "circle",
+                        "nodes": [vi[j] for j in layer_nodeidx[i]],
+                        "r": i + (1 if len(egos) != 1 else 0),
+                        "center": None if len(egos) != 1 else egos[0],
+                    }
+                )
+            graph.graph["constraints"] = circle_constraint
+            json.dump(
+                nx.node_link_data(graph),
+                open(filepath, "w", encoding="utf-8"),
+                indent=4,
+                ensure_ascii=False,
             )
-        # layer_constraints = []
-        # from collections import deque
 
-        # que = deque([v for v, _ in degree[: args.center_nodes_count]])
-        # visit = set([v for v, _ in degree[: args.center_nodes_count]])
-        # layer_constraints = []
-        # while que:
-        #     v = que.popleft()
-        #     for u in graph.neighbors(v):
-        #         if u not in visit:
-        #             visit.add(u)
-        #             que.append(u)
-        #             layer_constraints.append(
-        #                 {"axis": "y", "left": v, "right": u, "gap": 2}
-        #             )
+        if graph.graph.get("distance") is None:
+            dist = dict(nx.all_pairs_shortest_path_length(graph))
+            n = graph.number_of_nodes()
+            dist_matrix = [[0] * n for _ in range(n)]
+            for i, v in enumerate(list(graph.nodes)):
+                for j, u in enumerate(list(graph.nodes)):
+                    dist_matrix[i][j] = dist[v][u]
+            graph.graph["distance"] = dist_matrix
+            json.dump(
+                nx.node_link_data(graph),
+                open(filepath, "w", encoding="utf-8"),
+                indent=4,
+                ensure_ascii=False,
+            )
 
-        graph.graph["constraints"] = circle_constraint
         clusters = None
         if args.cluster_overlap_removal:
             clusters = [graph.nodes[u]["group"] for u in graph.nodes]
@@ -80,7 +84,6 @@ def main():
             clusters=clusters,
         )
         json.dump(pos, open(os.path.join(args.dest, basename), "w"), ensure_ascii=False)
-        # json.dump(nx.node_link_data(graph), open(f"data/graph/ego{rad}_{basename}"))
 
 
 if __name__ == "__main__":
