500bb1c5879803e555af7b6b8e9be55f6f015fcb
itochs <halglobe0108@gmail.com>
2025-09-05 22:04:04 +0900

    違反描画

--- scripts/plot.py
+++ scripts/plot.py
@@ -9,16 +9,18 @@ from util.graph.plot import plot_graph
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('graph_file')
-    parser.add_argument('drawing_file')
-    parser.add_argument('output')
+    parser.add_argument("graph_file")
+    parser.add_argument("drawing_file")
+    parser.add_argument("output")
+    parser.add_argument("--show-violation", action=argparse.BooleanOptionalAction)
+
     args = parser.parse_args()
 
     graph = nx.node_link_graph(json.load(open(args.graph_file)))
     pos = json.load(open(args.drawing_file))
     os.makedirs(os.path.dirname(args.output), exist_ok=True)
-    plot_graph(graph, pos, args.output)
+    plot_graph(graph, pos, args.output, show_violation=args.show_violation)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
