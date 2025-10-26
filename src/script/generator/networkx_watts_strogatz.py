04e8eb9cd7a4902a8acd30d5277a5811f86e7b8b
itochs <halglobe0108@gmail.com>
2025-10-26 20:09:53 +0900

    辺の長さを100にする

--- src/script/generator/networkx_watts_strogatz.py
+++ src/script/generator/networkx_watts_strogatz.py
@@ -9,6 +9,7 @@ class Arg(argparse.Namespace):
     node_number: int
     neighbor_number: int
     rewiring_prob: float
+    edge_length: float
 
 
 def main():
@@ -17,15 +18,21 @@ def main():
     parser.add_argument("--node-number", type=int, default=100)
     parser.add_argument("--neighbor-number", type=int, default=5)
     parser.add_argument("--rewiring-prob", type=float, default=0.5)
+    parser.add_argument("--edge-length", type=float, default=100.0)
     args = parser.parse_args(namespace=Arg)
 
-    graph = nx.connected_watts_strogatz_graph(
-        args.node_number, args.neighbor_number, args.rewiring_prob
-    )
-    assert nx.is_connected(graph)
+    for i in range(10):
+        graph = nx.newman_watts_strogatz_graph(
+            args.node_number, args.neighbor_number, args.rewiring_prob
+        )
+        if nx.is_connected(graph):
+            break
+    else:
+        raise ValueError("Could not generate a connected graph after 10 attempts.")
     graph = nx.relabel_nodes(graph, lambda x: str(x))
 
     distance = nx.floyd_warshall_numpy(graph, weight=None)
+    distance *= args.edge_length
     graph.graph["distance"] = distance.tolist()
     graph.graph["constraints"] = []
     data = nx.node_link_data(graph)
