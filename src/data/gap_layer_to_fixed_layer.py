a1be942f0f1f4fd711fd335d7145a954d6f83ade
itochs <halglobe0108@gmail.com>
2025-10-26 20:09:01 +0900

    グラフそのものを等号制約にするように変更

--- src/data/gap_layer_to_fixed_layer.py
+++ src/data/gap_layer_to_fixed_layer.py
@@ -1,6 +1,5 @@
 import argparse
 import json
-import os
 
 import networkx as nx
 
@@ -10,8 +9,6 @@ def main():
     parser.add_argument("--dest", default=".")
     parser.add_argument("input", nargs="+")
     args = parser.parse_args()
-    print(args.input)
-    print("test")
 
     for filepath in args.input:
         graph = nx.node_link_graph(json.load(open(filepath)), link="links")
@@ -48,9 +45,6 @@ def main():
         constraint = x_constraints + y_constraints
         graph.graph["constraints"] = constraint
 
-        basename = os.path.basename(filepath)
-        os.makedirs(args.dest, exist_ok=True)
-        filepath = os.path.join(args.dest, basename)
         with open(filepath, "w") as f:
             json.dump(nx.node_link_data(graph), f)
 
