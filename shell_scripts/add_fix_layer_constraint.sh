72aaa7ca36d6151e4951549074b981d9619506b6
itochs <halglobe0108@gmail.com>
2025-10-28 19:39:30 +0900

    階層の制約を並列化

--- shell_scripts/add_fix_layer_constraint.sh
+++ shell_scripts/add_fix_layer_constraint.sh
@@ -8,11 +8,11 @@ mkdir -p "$SAVE_DIR"
 
 for n in $(seq -f "%04g" 100 100 2000); do
 	echo "Processing node_$n.json ..."
-	for m in $(seq -f "%02g" 0 19); do
+	seq -f "%02g" 0 19 | parallel --jobs 8 "
 		python src/data/add_layer_constraint.py \
-			"$BASE_GRAPH_DIR"/"$n"/node_n="$n"_"$m".json \
-			"$SAVE_DIR"/"$n"/node_n="$n"_"$m".json
-	done
+			'$BASE_GRAPH_DIR'/'$n'/node_n='$n'_{}.json \
+			'$SAVE_DIR'/'$n'/node_n='$n'_{}.json
+	"
 	python src/data/gap_layer_to_fixed_layer.py \
 		"$SAVE_DIR"/"$n"/node_n="$n"_*.json
 done
