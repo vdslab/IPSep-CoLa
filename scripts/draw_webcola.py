3b9c3a9aa5da6db97ae051f1a241a78c6d0ad6f1
itochs <halglobe0108@gmail.com>
2025-12-23 19:57:59 +0900

    style: format draw_webcola.py with consistent quote style

--- scripts/draw_webcola.py
+++ scripts/draw_webcola.py
@@ -6,27 +6,32 @@ import subprocess
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dest', default='.')
-    parser.add_argument('--iterations', type=int, default=30)
-    parser.add_argument('--overlap-removal',
-                        action=argparse.BooleanOptionalAction)
-    parser.add_argument('--cluster-overlap-removal',
-                        action=argparse.BooleanOptionalAction)
-    parser.add_argument('input', nargs='+')
+    parser.add_argument("--dest", default=".")
+    parser.add_argument("--iterations", type=int, default=30)
+    parser.add_argument("--overlap-removal", action=argparse.BooleanOptionalAction)
+    parser.add_argument(
+        "--cluster-overlap-removal", action=argparse.BooleanOptionalAction
+    )
+    parser.add_argument("input", nargs="+")
     args = parser.parse_args()
 
     os.makedirs(args.dest, exist_ok=True)
     for filepath in args.input:
         basename = os.path.basename(filepath)
-        command = ['node', 'js/src/draw_webcola.js',
-                   '--graphFile', filepath,
-                   '--output', os.path.join(args.dest, basename)]
+        command = [
+            "node",
+            "js/src/draw_webcola.js",
+            "--graphFile",
+            filepath,
+            "--output",
+            os.path.join(args.dest, basename),
+        ]
         if args.overlap_removal:
-            command.append('--overlapRemoval')
+            command.append("--overlapRemoval")
         if args.cluster_overlap_removal:
-            command.append('--clusterOverlapRemoval')
+            command.append("--clusterOverlapRemoval")
         subprocess.run(command)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
